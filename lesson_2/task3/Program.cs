9f932714d1a376e77f73e587c8182958afb0209e
Anna Nikiforyak <a.nikiforyak@gmail.com>
2022-10-05 19:39:59 +0300

    added HW for lesson 2

---
+++ lesson_2/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void GetDayOff(int num)
+{
+    if (num > 7 || num < 1)
+    {
+        Console.WriteLine("такого дня недели нет");
+    }
+    else
+    {
+        string ans = num > 5 ? "да" : "нет";
+        Console.WriteLine(ans);
+    }
+}
+
+Console.WriteLine("Введите число:");
+GetDayOff(int.Parse(Console.ReadLine() ?? ""));
