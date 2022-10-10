e9a7ce710d5713be2edfd7bb14ef18c64ca68e55
Anna Nikiforyak <a.nikiforyak@gmail.com>
2022-10-10 14:39:03 +0300

    added HW for lesson 3

---
+++ lesson_3/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void IsPalindrome(int num)
+{
+    string str = num.ToString();
+
+
+    char[] arr = str.ToCharArray();
+    Array.Reverse(arr);
+    string rev_str = new String(arr);
+
+    string ans = str == rev_str ? "да" : "нет";
+    Console.WriteLine(ans);
+}
+
+Console.WriteLine("Введите пятизначное число:");
+IsPalindrome(int.Parse(Console.ReadLine() ?? ""));
---
+++ lesson_3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+double[] GetTableOfCubes(int n)
+{
+    double[] result = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = Math.Pow(i + 1, 3);
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+Console.WriteLine("{0}", string.Join(", ", GetTableOfCubes(int.Parse(Console.ReadLine() ?? ""))));
