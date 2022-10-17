a4d3a2600e699d412b9e90e3f15f29dc42b44a4e
Anna Nikiforyak <a.nikiforyak@gmail.com>
2022-10-17 16:09:43 +0300

    added HW for lesson 5

---
+++ lesson_5/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+
+int GetNumberEven(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int[] array = new int[6];
+array = FillArray(array);
+PrintArray(array);
+Console.WriteLine(GetNumberEven(array));
---
+++ lesson_5/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] GetArray(int len)
+{
+    double[] array = new double[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+
+double GetDiffBetweenMaxMinElements(double[] arr)
+{
+    double diff = arr.Max() - arr.Min();
+    return diff;
+}
+
+double[] array = GetArray(5);
+PrintArray(array);
+Console.WriteLine(GetDiffBetweenMaxMinElements(array));
